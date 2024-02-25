2595cdbbe02b48a12bdf23817c45b1ced1b0af82
Eli Fouts <116454864+elifouts@users.noreply.github.com>
2024-02-25 04:19:25 -0500

    Update hearts.py

--- hearts.py
+++ hearts.py
@@ -18,12 +18,9 @@ def Hearts_function(top, bottom, left, right, mode, target_value):
 
         wb = load_workbook(filename='GameGenie/Hearts.xlsx')
         ws = wb["Sheet1"]
-        #Showing Passing Order
-        print(current_pattern)#i want this to be on page 2 in info box
-        input("Press enter to continue...") #I want this to become a button you press on page 2
-        #Rounds
-        print(rounds)#I want this to be on page 2 in info box
-        #please import the chart to the pyside 2nd page so It can be edited
+        print(current_pattern)
+        input("Press enter to continue...")
+        print(rounds)    
         wb.save('GameGenie/Hearts.xlsx')
         temp_score = f"A{rounds}"
         top_points += int(ws[temp_score].value)
@@ -34,11 +31,10 @@ def Hearts_function(top, bottom, left, right, mode, target_value):
         temp_score = f"D{rounds}"
         left_points += int(ws[temp_score].value)
 
-        #Showing total scores
-        print(top_points) #I want This to display in a 1*4 chart in position 1
-        print(bottom_points) #I want this to display in the same chart at possition 2
-        print(left_points)#I want this to display in the same chart at possition 3
-        print(right_points)#I want this to display in the same chart at possition 4
+        print(top_points)
+        print(bottom_points) 
+        print(left_points)
+        print(right_points)
 
         if mode:
             if (top_points >=target_value and bottom_points >=target_value and left_points >=target_value and right_points >=target_value):
