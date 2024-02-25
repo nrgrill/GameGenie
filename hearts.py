e23e7569af19199defba00258f7ef2640ffc070e
Eli Fouts <116454864+elifouts@users.noreply.github.com>
2024-02-24 20:49:57 -0500

    UPDATES

--- hearts.py
+++ hearts.py
@@ -3,21 +3,11 @@ from openpyxl import Workbook, load_workbook
 wb = load_workbook(filename='GameGenie/Hearts.xlsx')
 ws = wb["Sheet1"]
 
-class Players:
-    def __init__(self):
-        self.players = []
-
-    def add_player(self, player_name):
-        if len(self.players) < 4:
-            self.players.append(player_name)
-
-player = Players()
-
 def Hearts(top, bottom, left, right, mode, target_value):
-    player.add_player(top)
-    player.add_player(bottom)
-    player.add_player(left)
-    player.add_player(right)
+    print(top)
+    print(bottom)
+    print(left)
+    print(right)
     top_points = 0
     bottom_points = 0
     left_points = 0
